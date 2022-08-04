239b9d62d2b4c852c271f93306d7276531cc44fa
Marc Kean <marc@marckean.com>
2022-08-04 16:14:35 +1000

    .

---
+++ Definitions/deploy-AzureJSONResources.ps1
@@ -0,0 +1,23 @@
+param (
+    $ManagementGroupId = "8efecb12-cbaa-4612-b850-e6a68c14d336",
+    $location = "australiaeast",
+    $ts_resourcegroupname = "TemplateSpecs"
+)
+
+New-AzTemplateSpec `
+  -Name 'TS_policyAssignments' `
+  -Version "2.0.2" `
+  -ResourceGroupName $ts_resourcegroupname `
+  -Location $location `
+  -TemplateFile ".\artifacts\policyAssignments.json" `
+  -Force
+
+  New-AzTemplateSpec `
+  -Name 'TS_policyDefinitions' `
+  -Version "2.0.2" `
+  -ResourceGroupName $ts_resourcegroupname `
+  -Location $location `
+  -TemplateFile ".\artifacts\policyDefinitions.json" `
+  -Force
+
+New-AzManagementGroupDeployment -Location $location -TemplateFile '.\deploy.json' -ManagementGroupId $ManagementGroupId -Verbose -ErrorAction Continue

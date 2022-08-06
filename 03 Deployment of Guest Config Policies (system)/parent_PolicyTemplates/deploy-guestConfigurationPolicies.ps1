15c97705f34bf85861f8de7f5e08e2a78b3bd8b8
Marc Kean <marc@marckean.com>
2022-08-06 13:22:33 +1000

    .

---
+++ 03 Deployment of Guest Config Policies (system)/parent_PolicyTemplates/deploy-guestConfigurationPolicies.ps1
@@ -0,0 +1,20 @@
+param (
+    $ManagementGroupId = "8efecb12-cbaa-4612-b850-e6a68c14d336",
+    $location = "australiaeast",
+    $ts_resourcegroupname = "TemplateSpecs"
+)
+
+New-AzTemplateSpec `
+  -Name 'Child_GuestConfigurationAssignmentsSystem' `
+  -Version "1.0.0" `
+  -ResourceGroupName $ts_resourcegroupname `
+  -Location $location `
+  -TemplateFile ".\03 Deployment of Guest Config Policies (system)\parent_PolicyTemplates\parentGuestConfiguration.json" `
+  -Force
+
+
+$TimeNow = Get-Date -Format yyyyMMdd-hhmm
+
+#New-AzManagementGroupDeployment -Location $location -TemplateFile '.\03 Deployment of Guest Config Policies (system)\parent_PolicyTemplates\parentGuestConfiguration.json' -ManagementGroupId $ManagementGroupId -Name $TimeNow -Verbose -ErrorAction Continue
+
+New-AzSubscriptionDeployment -Location $location -TemplateFile '.\03 Deployment of Guest Config Policies (system)\child_PolicyTemplates\policyAssignments.json' -Name $TimeNow -Verbose -ErrorAction Continue

7ae21fa0bf3a69f36bbf87b420da406fdf7178ff
Marc Kean <marc@marckean.com>
2022-08-17 11:48:17 +1000

    .

--- 04.1 Deployment of Guest Config VM Extension/parent_PolicyTemplates/deploy-guestConfigurationPolicies.ps1
+++ 04.1 Deployment of Guest Config VM Extension/parent_PolicyTemplates/deploy-guestConfigurationPolicies.ps1
@@ -1,9 +1,3 @@
-param (
-    $ManagementGroupId = "8efecb12-cbaa-4612-b850-e6a68c14d336",
-    $location = "australiaeast",
-    $ts_resourcegroupname = "TemplateSpecs"
-)
-
 $TimeNow = Get-Date -Format yyyyMMdd-hhmm
 
 New-AzResourceGroupDeployment -ResourceGroupName 'Company_IaaS' -TemplateFile '.\04.1 Deployment of Guest Config VM Extension\child_PolicyTemplates\policyAssignments.json' -Name $TimeNow -Verbose -ErrorAction Continue

88d334dc1ae040ff1b40c454fecf879843d4e834
Marc Kean <marc@marckean.com>
2022-08-30 18:27:36 +1000

    .

--- 05 Deployment of Guest Config VM Extension/parent_PolicyTemplates/deploy-guestConfigurationPolicies.ps1
+++ 05 Deployment of Guest Config VM Extension/parent_PolicyTemplates/deploy-guestConfigurationPolicies.ps1
@@ -1,10 +1,15 @@
 $TimeNow = Get-Date -Format yyyyMMdd-hhmm
 
-New-AzResourceGroupDeployment -ResourceGroupName 'Company_IaaS' -TemplateFile '.\04.1 Deployment of Guest Config VM Extension\child_PolicyTemplates\policyAssignments.json' -Name $TimeNow -Verbose -ErrorAction Continue
+New-AzResourceGroupDeployment -ResourceGroupName 'Company_IaaS' -TemplateFile '.\05 Deployment of Guest Config VM Extension\child_PolicyTemplates\policyAssignments.json' -Name $TimeNow -Verbose -ErrorAction Continue
 
 
 
-######## Testing - do not run
+
+
+
+#--------------------------------------------
+# Testing - DO NOT RUN
+#--------------------------------------------
 $lastDeploymentName = (Get-AzSubscriptionDeployment | where {$_.ProvisioningState -ne 'Succeeded'} | sort Timestamp -Descending)[0].DeploymentName
 if($lastDeploymentName){Remove-AzSubscriptionDeployment -Name $lastDeploymentName}
 
